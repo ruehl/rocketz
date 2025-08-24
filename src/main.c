7f6e44ba6f3653894fbee025b70fb082254187c9
Christian Ruehl <ruehl@objectix.de>
2025-08-24 18:53:21 +0000

    added simple update loop

--- src/main.c
+++ src/main.c
@@ -1,11 +1,44 @@
 #include <ncurses.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "models/rigid_object.h"
+
+void update(unsigned int dt) {
+	
+}
 
 int main() {
-    initscr();
+	RigidObject obj;
+	obj.altitude = 1000.0f;
+	obj.velocity = 50.0f;
+	obj.mass = 75.0f;
+	obj.gravity = 9.81f;
+
+	struct timespec last_time;
+	clock_gettime(CLOCK_MONOTONIC, &last_time);
+
+	initscr();
+
+	while(true) {
+		// update delta time
+		struct timespec current_time;
+		clock_gettime(CLOCK_MONOTONIC, &current_time);
+		unsigned int dt = (current_time.tv_sec - last_time.tv_sec) * 1000 + (current_time.tv_nsec - last_time.tv_nsec) / 1000000;
+
+		// update game state for given dt
+		update(dt);
+		last_time = current_time;
+
+		clear();
+		printw("%d", dt);
+		refresh();
+
+		// wait for short time to achieve a fps of 60hz
+		usleep(16667);
+	}
     
-	printw("Hello World !!!");
-	refresh();
-	getch();
 	endwin();
 
-	return 0;}
+	return 0;
+}
---
+++ src/models/rigid_object.h
@@ -0,0 +1,6 @@
+typedef struct {
+    double altitude; // meters
+    double velocity; // meters per second
+    double mass;     // kilograms
+    double gravity;  // meters per second squared
+} RigidObject;
